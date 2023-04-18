5856fb40229d3ca03490509b5db99cdd89dd304b
Marco Ventura <130122725+Marco-Ventura@users.noreply.github.com>
2023-04-18 14:22:21 -0700

    first commit of my-app project

---
+++ src/componentes/testimonio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export function Testimonio(){
+  return (
+    <div className='contenedor-testimonio'>
+      <img className='imagen-testimonio'
+            src={require('../imagenes/testimonio1.png')} 
+            alt='foto de hombre'/>
+      <div className='texto-testimonio-contenedor'>
+        <p className='nombre-testimonio'> Enma Watson en Paris </p>
+        <p className='cargo-testimonio'>Ingeniera en Spotify</p>
+        <p className='texto-testimonio'>ella a sido infdfd dkkdddk  kddkdjj dkdjndjd djdk kdj dkd kdkdk ext analytics is a fantastic tool for delivering insight over high volumes of text data, and multiple sources. And its automated brain allows it to do this in a quick and scalable manner. However, it still needs a human heart to deliver meaning and interpretation to the results. ... On November 18, join experts from Ipsos, Convosphere and BT for a discussion about what social intelligence research is, and how best to communicate the benefit within your organization.</p>
+        
+        
+      </div>      
+
+    </div>
+  );
+}
+
